d19d2458b156c5a2c8cde6bc4b961e239c7756c0
薄奚梦灵 <boximengling@outlook.com>
2025-05-26 14:01:05 +0800

    fix: 反序列化 Json 会报错

--- SuikaiLauncher.Core.Base/Modules/Json.cs
+++ SuikaiLauncher.Core.Base/Modules/Json.cs
@@ -14,34 +14,17 @@ namespace SuikaiLauncher.Core
 
             try
             {
-                
-                var node = JsonNode.Parse(jsonText)
-                           ?? throw new ArgumentException("格式化 Json 失败", nameof(jsonText));
-
-                
-                return node.Deserialize<T>()
-                       ?? throw new JsonException($"无法将 JSON 反序列化为 {typeof(T).Name}");
+                var options = new JsonSerializerOptions
+                {
+                    TypeInfoResolver = new System.Text.Json.Serialization.Metadata.DefaultJsonTypeInfoResolver()
+                };
+                return JsonSerializer.Deserialize<T>(jsonText,options)
+                       ?? throw new JsonException($"反序列化 Json 失败：无法转换为 {typeof(T).Name}");
             }
-            catch (Exception ex) when (
-                ex is JsonException || ex is ArgumentException || ex is ArgumentNullException)
+            catch (Exception ex)
             {
                 Logger.Log(ex, "无效的 Json 结构或转换失败");
                 throw;
-            }
-        }
-        public static List<string> GetJsonArray(JsonNode? jsonNode){
-            List<string> DataList = new();
-            try{
-                
-                if (jsonNode is null) return DataList;
-                foreach (var Data in jsonNode.AsArray()){
-                    if(Data is null) continue;
-                    DataList.Add(Data.ToString());
-                }
-                return DataList;
-            }catch{
-                if(jsonNode is not null) DataList.Add(jsonNode.ToString());
-                return DataList;
             }
         }
     }
